dd59109f0fd3ce0d75f1ddf5b03ca12aaf8a642e
Jeremy Solbrig <jeremy.solbrig@colostate.edu>
2023-05-10 15:35:47 -0600

    Update test data paths

--- tests/scripts/abi_cldFraction.sh
+++ tests/scripts/abi_cldFraction.sh
@@ -16,14 +16,14 @@ script_dir=`dirname $0`
 output_dir=$script_dir/../outputs
 
 run_procflow \
-    $GEOIPS_TESTDATA_DIR/test_data_clavrx/data/goes16_20230113_0000/clavrx_goes16_20230113000020.level2.hdf \
+    $GEOIPS_TESTDATA_DIR/test_data_clavrx/data/goes16_2023101_1600/clavrx_OR_ABI-L1b-RadF-M6C01_G16_s20231011600207.level2.hdf \
     --procflow single_source \
     --reader_name clavrx_hdf4 \
     --product_name cldFraction \
     --output_formatter imagery_clean \
     --compare_path "$output_dir/abi.<product>.imagery_clean" \
     --minimum_coverage 0 \
-    --sector_list goes16 
+    --sector_list goes16
 retval=$?
 
 exit $retval
--- tests/scripts/ahi_cldMask.sh
+++ tests/scripts/ahi_cldMask.sh
@@ -15,14 +15,14 @@
 script_dir=`dirname $0`
 output_dir=$script_dir/../outputs
 
-run_procflow $GEOIPS_TESTDATA_DIR/test_data_clavrx/data/himawari8_20201201_0900/clavrx_H08_202012010900.level2.hdf \
+run_procflow $GEOIPS_TESTDATA_DIR/test_data_clavrx/data/himawari9_2023101_0300/clavrx_H09_20230411_0300_B01_FLDK_DK_R10_S0110.DAT.level2.hdf \
     --procflow single_source \
     --reader_name clavrx_hdf4 \
     --product_name cldMask \
     --output_formatter imagery_clean \
     --compare_path "$output_dir/ahi.<product>.imagery_clean" \
     --minimum_coverage 0 \
-    --sector_list himawari8 
+    --sector_list himawari8
 retval=$?
 
 exit $retval
