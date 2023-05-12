3064b1bbd636a977e9761e5d8311213fbe2e1dbe
mindyls <mindyls@users.noreply.github.com>
2023-05-12 06:16:16 -0700

    Updating product names in test script calls

--- tests/scripts/abi.Cloud-Fraction.imagery_clean.sh
+++ tests/scripts/abi.Cloud-Fraction.imagery_clean.sh
@@ -19,7 +19,7 @@ run_procflow \
     $GEOIPS_TESTDATA_DIR/test_data_clavrx/data/goes16_2023101_1600/clavrx_OR_ABI-L1b-RadF-M6C01_G16_s20231011600207.level2.hdf \
     --procflow single_source \
     --reader_name clavrx_hdf4 \
-    --product_name cldFraction \
+    --product_name Cloud-Fraction \
     --output_formatter imagery_clean \
     --compare_path "$output_dir/abi.<product>.imagery_clean" \
     --minimum_coverage 0 \
