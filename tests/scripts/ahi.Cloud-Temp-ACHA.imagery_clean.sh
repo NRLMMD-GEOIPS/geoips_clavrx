286b98c9c9c94ccfcd9992e1aa4d770b35256fe6
mindyls <mindyls@users.noreply.github.com>
2023-05-11 17:25:21 -0700

    Finalize temp script names and products
    cldTemp -> CLoud-Temp-ACHA
    Temp11p0 -> Temp-11p0
    Temp3p75 -> Temp-3p75

--- tests/scripts/ahi.Cloud-Temp-ACHA.imagery_clean.sh
+++ tests/scripts/ahi.Cloud-Temp-ACHA.imagery_clean.sh
@@ -18,7 +18,7 @@ output_dir=$script_dir/../outputs
 run_procflow $GEOIPS_TESTDATA_DIR/test_data_clavrx/data/himawari9_2023101_0300/clavrx_H09_20230411_0300_B01_FLDK_DK_R10_S0110.DAT.level2.hdf \
     --procflow single_source \
     --reader_name clavrx_hdf4 \
-    --product_name cldTemp \
+    --product_name Cloud-Temp-ACHA \
     --output_formatter imagery_clean \
     --compare_path "$output_dir/ahi.<product>.imagery_clean" \
     --minimum_coverage 0 \
