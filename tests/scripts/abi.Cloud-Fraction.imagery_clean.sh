8c56a928a088a2c1b4633e0766e060f1ef3d34ad
mindyls <13873624+mindyls@users.noreply.github.com>
2023-08-01 10:49:13 -0700

    1.10.2 release NRLMMD-GEOIPS/geoips#234 (#22)
    Co-authored-by: mindyls <mindyls@users.noreply.github.com>

---
+++ tests/scripts/abi.Cloud-Fraction.imagery_clean.sh
@@ -0,0 +1,29 @@
+# # # Distribution Statement A. Approved for public release. Distribution unlimited.
+# # #
+# # # Author:
+# # # Naval Research Laboratory, Marine Meteorology Division
+# # #
+# # # This program is free software: you can redistribute it and/or modify it under
+# # # the terms of the NRLMMD License included with this program. This program is
+# # # distributed WITHOUT ANY WARRANTY; without even the implied warranty of
+# # # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the included license
+# # # for more details. If you did not receive the license, for more information see:
+# # # https://github.com/U-S-NRL-Marine-Meteorology-Division/
+
+#!/bin/bash
+
+script_dir=`dirname $0`
+output_dir=$script_dir/../outputs
+
+run_procflow \
+    $GEOIPS_TESTDATA_DIR/test_data_clavrx/data/goes16_2023101_1600/clavrx_OR_ABI-L1b-RadF-M6C01_G16_s20231011600207.level2.hdf \
+    --procflow single_source \
+    --reader_name clavrx_hdf4 \
+    --product_name Cloud-Fraction \
+    --output_formatter imagery_clean \
+    --compare_path "$output_dir/abi.<product>.imagery_clean" \
+    --minimum_coverage 0 \
+    --sector_list goes16
+retval=$?
+
+exit $retval
--- tests/test_all.sh
+++ tests/test_all.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # # # Distribution Statement A. Approved for public release. Distribution unlimited.
 # # #
 # # # Author:
@@ -10,45 +11,48 @@
 # # # for more details. If you did not receive the license, for more information see:
 # # # https://github.com/U-S-NRL-Marine-Meteorology-Division/
 
-# Please identify and update all instances of "@" found in this script appropriately.
-# You will need to generate one or more test scripts that test your complete functionality,
-# (these scripts provide example geoips calls and sample output, as well as providing a full integration test),
-# and call each script within the test_all.sh script.  Do not rename this script or test directory - automated
-# integrations tests look for the tests/test_all.sh script for complete testing.
-
-#!/bin/bash
-
-# This should contain test calls to cover ALL required functionality tests for the @package@ repo.
+# This should contain test calls to cover ALL required functionality tests for
+# the geoips_clavrx repo.
 
 # The $GEOIPS tests modules sourced within this script handle:
-   # setting up the appropriate associative arrays for tracking the overall return value,
-   # calling the test scripts appropriately, and 
+   # setting up the appropriate associative arrays for tracking the overall
+   #   return value,
+   # calling the test scripts appropriately, and
    # setting the final return value.
 
-# Note you must use the variable "call" in the for the loop
+if [[ ! -d $GEOIPS_PACKAGES_DIR/geoips ]]; then
+    echo "Must CLONE geoips repository into \$GEOIPS_PACKAGES_DIR location"
+    echo "to use test_all.sh testing utility."
+    echo ""
+    echo "export GEOIPS_PACKAGES_DIR=<path_to_geoips_cloned_packages>"
+    echo "git clone https://github.com/NRLMMD-GEOIPS/geoips $GEOIPS_PACKAGES_DIR/geoips"
+    echo ""
+    exit 1
+fi
 
 # Argument to test_all_pre.sh ONLY sets the prefix on the log output / filenames.
-# Used for clarity, and to differentiate potentially multiple "test_all.sh" scripts in the same repo.
+# Used for clarity, and to differentiate potentially multiple "test_all.sh" scripts
+# in the same repo.
 . $GEOIPS_PACKAGES_DIR/geoips/tests/utils/test_all_pre.sh geoips_clavrx
 
 echo ""
+# Note you must use the variable "call" in the for the loop
 # "call" used in test_all_run.sh
 for call in \
 \
     "$GEOIPS_PACKAGES_DIR/geoips/tests/utils/check_code.sh all `dirname $0`/../" \
-    "$GEOIPS_PACKAGES_DIR/geoips_clavrx/tests/scripts/abi_cldFraction.sh" \
-    "$GEOIPS_PACKAGES_DIR/geoips_clavrx/tests/scripts/ahi_cldFraction.sh" \
-    "$GEOIPS_PACKAGES_DIR/geoips_clavrx/tests/scripts/ahi_cldHeightBase.sh" \
-    "$GEOIPS_PACKAGES_DIR/geoips_clavrx/tests/scripts/ahi_cldHeight.sh" \
-    "$GEOIPS_PACKAGES_DIR/geoips_clavrx/tests/scripts/ahi_cldHeightTop.sh" \
-    "$GEOIPS_PACKAGES_DIR/geoips_clavrx/tests/scripts/ahi_cldMask.sh" \
-    "$GEOIPS_PACKAGES_DIR/geoips_clavrx/tests/scripts/ahi_cldOpd.sh" \
-    "$GEOIPS_PACKAGES_DIR/geoips_clavrx/tests/scripts/ahi_cldPhase.sh" \
-    "$GEOIPS_PACKAGES_DIR/geoips_clavrx/tests/scripts/ahi_cldReff.sh" \
-    "$GEOIPS_PACKAGES_DIR/geoips_clavrx/tests/scripts/ahi_cldTemp.sh" \
-    "$GEOIPS_PACKAGES_DIR/geoips_clavrx/tests/scripts/ahi_cldType.sh" \
-    "$GEOIPS_PACKAGES_DIR/geoips_clavrx/tests/scripts/ahi_Temp11p0.sh" \
-    "$GEOIPS_PACKAGES_DIR/geoips_clavrx/tests/scripts/ahi_Temp3p75.sh"
+    "$GEOIPS_PACKAGES_DIR/geoips_clavrx/tests/scripts/abi.Cloud-Fraction.imagery_clean.sh" \
+    "$GEOIPS_PACKAGES_DIR/geoips_clavrx/tests/scripts/ahi.Cloud-Fraction.imagery_clean.sh" \
+    "$GEOIPS_PACKAGES_DIR/geoips_clavrx/tests/scripts/ahi.Cloud-Top-Height.imagery_clean.sh" \
+    "$GEOIPS_PACKAGES_DIR/geoips_clavrx/tests/scripts/ahi.Cloud-Base-Height.imagery_clean.sh" \
+    "$GEOIPS_PACKAGES_DIR/geoips_clavrx/tests/scripts/ahi.Cloud-Mask.imagery_clean.sh" \
+    "$GEOIPS_PACKAGES_DIR/geoips_clavrx/tests/scripts/ahi.Cloud-Optical-Depth.imagery_clean.sh" \
+    "$GEOIPS_PACKAGES_DIR/geoips_clavrx/tests/scripts/ahi.Cloud-Phase.imagery_clean.sh" \
+    "$GEOIPS_PACKAGES_DIR/geoips_clavrx/tests/scripts/ahi.Effective-Radius.imagery_clean.sh" \
+    "$GEOIPS_PACKAGES_DIR/geoips_clavrx/tests/scripts/ahi.Cloud-Temp-ACHA.imagery_clean.sh" \
+    "$GEOIPS_PACKAGES_DIR/geoips_clavrx/tests/scripts/ahi.Cloud-Type.imagery_clean.sh" \
+    "$GEOIPS_PACKAGES_DIR/geoips_clavrx/tests/scripts/ahi.Temp-11p0.imagery_clean.sh" \
+    "$GEOIPS_PACKAGES_DIR/geoips_clavrx/tests/scripts/ahi.Temp-3p75.imagery_clean.sh"
 do
     . $GEOIPS_PACKAGES_DIR/geoips/tests/utils/test_all_run.sh
 done
