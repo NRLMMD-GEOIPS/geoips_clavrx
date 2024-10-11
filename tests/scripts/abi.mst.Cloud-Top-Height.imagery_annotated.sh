a889f41baa6cc1bcb2e51a2504453907fbc069b1
evrose54 <mrewrose@gmail.com>
2024-10-11 18:14:32 +0000

    updated test data location for new test script

--- tests/scripts/abi.mst.Cloud-Top-Height.imagery_annotated.sh
+++ tests/scripts/abi.mst.Cloud-Top-Height.imagery_annotated.sh
@@ -12,7 +12,7 @@
 
 #!/bin/bash
 
-geoips run single_source $GEOIPS_TESTDATA_DIR/reader_tests/clavrx/goes16/2024283.1500.1510/*.hdf \
+geoips run single_source $GEOIPS_TESTDATA_DIR/reader_tests/data/clavrx/goes16/2024283.1500.1510/*.hdf \
     --reader_name clavrx_hdf4 \
     --product_name Absdiff-Cloud-Top-Height \
     --output_formatter imagery_annotated \
