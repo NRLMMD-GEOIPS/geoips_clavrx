7df6f3b1b2066e492b0dfcc630d8bd171ca576fe
Evan Rose <62626811+evrose54@users.noreply.github.com>
2024-11-16 00:55:28 -0700

    CLAVR-x Reader Refactor: Multi Scan Time Support (#47)
    * updated reader for multi scan times and made a new product to demonstrate this
    
    * updated call of clavrx_hdf4 to use new function on the readers interface
    
    * added release notes to this PR
    
    * fixed spelling mistake in release notes
    
    * updated test data location for new test script
    
    * updated plugins to reflect changes requested on remote
    
    * fixed flake8 errors
    
    * reverted sorted(fnames) change as we made that occur in the readers interface instead)
    
    * updated path to test data used for this script
    
    * updated test data path; was incorrect
    
    ---------
    
    Co-authored-by: Jeremy Solbrig <1684491+jsolbrig@users.noreply.github.com>

---
+++ tests/scripts/abi.mst.Cloud-Top-Height.imagery_annotated.sh
@@ -0,0 +1,23 @@
+# # # Distribution Statement A. Approved for public release. Distribution unlimited.
+# # #
+# # # Author:
+# # # Naval Research Laboratory, Marine Meteorology Division
+# # #
+# # # This program is free software: you can redistribute it and/or modify it under
+# # # the terms of the NRLMMD License included with this program. This program is
+# # # distributed WITHOUT ANY WARRANTY; without even the implied warranty of
+# # # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the included license
+# # # for more details. If you did not receive the license, for more information see:
+# # # https://github.com/U-S-NRL-Marine-Meteorology-Division/
+
+#!/bin/bash
+
+geoips run single_source $GEOIPS_TESTDATA_DIR/test_data_clavrx/data/goes16_2024283_1501_1506/*.hdf \
+    --reader_name clavrx_hdf4 \
+    --product_name Absdiff-Cloud-Top-Height \
+    --output_formatter imagery_annotated \
+    --minimum_coverage 0 \
+    --sector_list conus
+retval=$?
+
+exit $retval
